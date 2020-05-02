53621b4ea96b21d81dc4c8c72eaab8172b46344e
Saúl Piña <sauljabin@gmail.com>
2020-05-02 15:04:51 -0500

    Adding git check :octocat:

--- gitignore.py
+++ gitignore.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python
 
-print("Hola mundo")
+import os
+
+version = "0.1.0"
+
+commands = {
+    "git_version": "git --version"
+}
+
+sources = {
+    "github.com": "https://github.com/github/gitignore.git",
+    "gitignore.io": "https://github.com/toptal/gitignore.git"
+}
+
+
+def exec_command(command, output=False):
+    return os.system("{}{}".format(commands[command], "" if output else " > /dev/null 2>&1"))
+
+
+def check_git():
+    if exec_command("git_version") != 0:
+        raise Exception("Install git first")
+
+
+def download_sources():
+    return
+
+
+try:
+    check_git()
+except Exception as e:
+    print(e.message)
