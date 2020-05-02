3a45e8f1948e5b4145a5fa134b9cf7b0717130f3
Saúl Piña <sauljabin@gmail.com>
2020-05-02 18:09:03 -0500

    Adding restart script in case update

--- gitignore.py
+++ gitignore.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import sys
 
 version = "0.1.0"
 script_path = os.path.dirname(__file__)
@@ -12,6 +13,15 @@ sources = {
 }
 
 default_source = sources.keys()[0]
+
+
+def get_path(*to_join):
+    return os.path.join(script_path, *to_join)
+
+
+def get_command_output(command, *parameters):
+    to_exec = "{} {}".format(command, " ".join(parameters))
+    return os.popen(to_exec).read().strip()
 
 
 def exec_command(command, *parameters):
@@ -28,7 +38,7 @@ def check_git():
 
 def download_sources():
     for source, remote_path in sources.items():
-        local_path = os.path.join(script_path, source)
+        local_path = get_path(script_path, source)
         if exec_command("git -C", local_path, "status") != 0:
             exec_command("git clone", remote_path, local_path)
         else:
@@ -36,27 +46,31 @@ def download_sources():
 
 
 def update_script():
+    current_hash = get_command_output("git -C", script_path, "rev-parse HEAD")
     exec_command("git -C", script_path, "pull")
+    new_hash = get_command_output("git -C", script_path, "rev-parse HEAD")
+    if current_hash != new_hash:
+        os.execv(__file__, sys.argv)
 
 
 def process_clean():
     if args.clean:
         for source in sources:
-            local_path = os.path.join(script_path, source)
+            local_path = get_path(script_path, source)
             exec_command("rm -rf", local_path)
 
 
 def setup_args():
-    parser = argparse.ArgumentParser(description="Generates .gitignore files from templates", prog='gitignore')
+    parser = argparse.ArgumentParser(description="Generates .gitignore files from templates", prog="gitignore")
 
-    parser.add_argument("-d", "--debug", help="print full output", action='store_true')
-    parser.add_argument("-c", "--clean", help="clean sources", action='store_true')
-    parser.add_argument("-a", "--append", help="append to existing .gitignore file", action='store_true')
+    parser.add_argument("-d", "--debug", help="print full output", action="store_true")
+    parser.add_argument("-c", "--clean", help="clean sources", action="store_true")
+    parser.add_argument("-a", "--append", help="append to existing .gitignore file", action="store_true")
     parser.add_argument("keys", help="IDEs, Languages or OSs, accepts multiple", type=str, nargs="+")
     parser.add_argument("-s", "--source",
                         help="select template source, default: " + default_source,
                         type=str, nargs="?", default=default_source, choices=sources.keys())
-    parser.add_argument("-v", "--version", action='version', version="%(prog)s " + version)
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s " + version)
 
     return parser.parse_args()
 
