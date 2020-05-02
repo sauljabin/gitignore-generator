8af47744804279b4459c01ba4b1454f4e9706b2f
Saúl Piña <sauljabin@gmail.com>
2020-05-02 17:27:11 -0500

    Adding args :alien:

--- gitignore.py
+++ gitignore.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 
+import argparse
 import os
 
 version = "0.1.0"
-
-debug = True
 script_path = os.path.dirname(__file__)
 
 sources = {
-    "github.com": "https://github.com/github/gitignore.git",
-    "gitignore.io": "https://github.com/toptal/gitignore.git"
+    "gitignore.io": "https://github.com/toptal/gitignore.git",
+    "github.com": "https://github.com/github/gitignore.git"
 }
+
+default_source = sources.keys()[0]
 
 
 def exec_command(command, *parameters):
-    to_exec = "{} {} {}".format(command, " ".join(parameters), "" if debug else "> /dev/null 2>&1")
-    if debug:
+    to_exec = "{} {} {}".format(command, " ".join(parameters), "" if args.debug else "> /dev/null 2>&1")
+    if args.debug:
         print(to_exec)
     return os.system(to_exec)
 
@@ -38,7 +39,23 @@ def update_script():
     exec_command("git -C", script_path, "pull")
 
 
+def setup_args():
+    parser = argparse.ArgumentParser(description="Generates .gitignore files from templates", prog='gitignore')
+
+    parser.add_argument("-d", "--debug", help="print full output", action='store_true')
+    parser.add_argument("-c", "--clean", help="clean sources", action='store_true')
+    parser.add_argument("-a", "--append", help="append to existing .gitignore file", action='store_true')
+    parser.add_argument("keys", help="IDEs, Languages or OSs, accepts multiple", type=str, nargs="+")
+    parser.add_argument("-s", "--source",
+                        help="select template source, default: " + default_source,
+                        type=str, nargs="?", default=default_source, choices=sources.keys())
+    parser.add_argument("-v", "--version", action='version', version="%(prog)s " + version)
+
+    return parser.parse_args()
+
+
 try:
+    args = setup_args()
     check_git()
     update_script()
     download_sources()
